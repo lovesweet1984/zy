61778757caac36fa3e72c5d0e5305b1b0e814886
lovesweet1984 <lovesweet1984@gmail.com>
2024-03-27 21:41:51 +0800

    1

--- wireguard-pz/install.sh
+++ wireguard-pz/install.sh
@@ -12,11 +12,11 @@ sudo bash wireguard.sh --auto
 
 echo -e "开始设置wireguard"
 sudo systemctl stop wg-quick@wg0.service
-curl -o /root/client.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/client.conf
-curl -o /root/1.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/1.conf
-curl -o /root/2.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/2.conf
-curl -o /root/3.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/3.conf
-curl -o /etc/wireguard/wg0.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/wg0.conf
+sudo curl -o /root/client.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/client.conf
+sudo curl -o /root/1.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/1.conf
+sudo curl -o /root/2.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/2.conf
+sudo curl -o /root/3.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/3.conf
+sudo curl -o /etc/wireguard/wg0.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/wg0.conf
 sudo systemctl restart wg-quick@wg0.service
 sudo systemctl status wg-quick@wg0.service
 echo -e "完成设置wirguard"
