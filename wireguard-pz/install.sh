29f43f0d9109ba502f5bbc80a16beca7e8f0d2b7
lovesweet1984 <138853127+lovesweet1984@users.noreply.github.com>
2024-03-27 19:16:58 +0800

    Add files via upload

---
+++ wireguard-pz/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo -e "开始自动安装wirguard"
+wget -O wireguard.sh https://get.vpnsetup.net/wg && sudo bash wireguard.sh --auto
+
+
+
+echo -e "开始设置wireguard"
+curl -o /root/client.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/client.conf
+curl -o /root/1.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/1.conf
+curl -o /root/2.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/2.conf
+curl -o /root/3.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/3.conf
+curl -o /etc/wireguard/wg0.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/wg0.conf
+sudo systemctl restart wg-quick@wg0.service
+sudo systemctl stop ufw.service
+sudo systemctl disable ufw.service
+sudo ufw status
+echo -e "完成设置wirguard"
+
+
+
+
