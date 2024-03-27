32d07f0c15c970b78fcb4ce99cc8db5c3f443791
lovesweet1984 <lovesweet1984@gmail.com>
2024-03-27 19:38:33 +0800

    1

--- wireguard-pz/install.sh
+++ wireguard-pz/install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 echo -e "开始自动安装wirguard"
-wget -O wireguard.sh https://get.vpnsetup.net/wg && sudo bash wireguard.sh --auto
+wget -O wireguard.sh https://get.vpnsetup.net/wg
+sudo bash wireguard.sh --auto
 
 
 
 echo -e "开始设置wireguard"
+sudo systemctl stop wg-quick@wg0.service
 curl -o /root/client.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/client.conf
 curl -o /root/1.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/1.conf
 curl -o /root/2.conf https://raw.githubusercontent.com/lovesweet1984/zy/main/wireguard-pz/2.conf
