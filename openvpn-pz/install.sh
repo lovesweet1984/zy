d422117ac685519c6085310e9d13a624e7fb8d20
lovesweet1984 <lovesweet1984@gmail.com>
2024-04-03 04:18:33 +0800

    更新 install.sh

--- openvpn-pz/install.sh
+++ openvpn-pz/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+echo -e "开始关闭防火墙"
+sudo systemctl stop ufw.service
+sudo systemctl disable ufw.service
+sudo ufw status
+
 echo -e "开始自动安装openvpn"
 wget -O openvpn.sh https://get.vpnsetup.net/ovpn && sudo bash openvpn.sh --auto
 
@@ -12,7 +18,6 @@ sudo sed -i 's/8.8.4.4/1.0.0.1/' /etc/openvpn/server/server.conf
 
 echo -e "开始移动openvpn的配置文件"
 cp -r /root/zy/openvpn-pz/server /etc/openvpn
-
 
 echo -e "重启服务器"
 reboot
