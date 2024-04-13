2915dd0f8a15c0bf08b15bd417b4e8f247f59daa
lovesweet1984 <lovesweet1984@gmail.com>
2024-04-14 03:55:44 +0800

    1

---
+++ bbr/bbrplus+fq.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+remove_all
+installbbrplus
+startbbrplus
+reboot
+
+
+#卸载全部加速
+remove_all() {
+  rm -rf /etc/sysctl.d/*.conf
+  #rm -rf /etc/sysctl.conf
+  #touch /etc/sysctl.conf
+  if [ ! -f "/etc/sysctl.conf" ]; then
+    touch /etc/sysctl.conf
+  else
+    cat /dev/null >/etc/sysctl.conf
+  fi
+  sysctl --system
+  sed -i '/DefaultTimeoutStartSec/d' /etc/systemd/system.conf
+  sed -i '/DefaultTimeoutStopSec/d' /etc/systemd/system.conf
+  sed -i '/DefaultRestartSec/d' /etc/systemd/system.conf
+  sed -i '/DefaultLimitCORE/d' /etc/systemd/system.conf
+  sed -i '/DefaultLimitNOFILE/d' /etc/systemd/system.conf
+  sed -i '/DefaultLimitNPROC/d' /etc/systemd/system.conf
+
+  sed -i '/soft nofile/d' /etc/security/limits.conf
+  sed -i '/hard nofile/d' /etc/security/limits.conf
+  sed -i '/soft nproc/d' /etc/security/limits.conf
+  sed -i '/hard nproc/d' /etc/security/limits.conf
+
+  sed -i '/ulimit -SHn/d' /etc/profile
+  sed -i '/ulimit -SHn/d' /etc/profile
+  sed -i '/required pam_limits.so/d' /etc/pam.d/common-session
+
+  systemctl daemon-reload
+
+  rm -rf bbrmod
+  sed -i '/net.ipv4.tcp_retries2/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
+  sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+  sed -i '/fs.file-max/d' /etc/sysctl.conf
+  sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+  sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+  sed -i '/net.core.rmem_default/d' /etc/sysctl.conf
+  sed -i '/net.core.wmem_default/d' /etc/sysctl.conf
+  sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+  sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_tw_recycle/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+  sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
+  sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+  sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
+  if [[ -e /appex/bin/lotServer.sh ]]; then
+    bash <(wget -qO- https://raw.githubusercontent.com/fei5seven/lotServer/master/lotServerInstall.sh) uninstall
+  fi
+  clear
+  echo -e "${Info}:清除加速完成。"
+  sleep 1s
+}
+
+
+
+#安装BBRplus内核 4.14.129
+installbbrplus() {
+  kernel_version="4.14.160-bbrplus"
+  bit=$(uname -m)
+  rm -rf bbrplus
+  mkdir bbrplus && cd bbrplus || exit
+  if [[ "${OS_type}" == "CentOS" ]]; then
+    if [[ ${version} == "7" ]]; then
+      if [[ ${bit} == "x86_64" ]]; then
+        kernel_version="4.14.129_bbrplus"
+        detele_kernel_head
+        headurl=https://github.com/cx9208/Linux-NetSpeed/raw/master/bbrplus/centos/7/kernel-headers-4.14.129-bbrplus.rpm
+        imgurl=https://github.com/cx9208/Linux-NetSpeed/raw/master/bbrplus/centos/7/kernel-4.14.129-bbrplus.rpm
+
+        headurl=$(check_cn $headurl)
+        imgurl=$(check_cn $imgurl)
+
+        download_file $headurl kernel-headers-c7.rpm
+        download_file $imgurl kernel-c7.rpm
+        yum install -y kernel-c7.rpm
+        yum install -y kernel-headers-c7.rpm
+      else
+        echo -e "${Error} 不支持x86_64以外的系统 !" && exit 1
+      fi
+    fi
+
+  elif [[ "${OS_type}" == "Debian" ]]; then
+    if [[ ${bit} == "x86_64" ]]; then
+      kernel_version="4.14.129-bbrplus"
+      detele_kernel_head
+      headurl=https://github.com/cx9208/Linux-NetSpeed/raw/master/bbrplus/debian-ubuntu/x64/linux-headers-4.14.129-bbrplus.deb
+      imgurl=https://github.com/cx9208/Linux-NetSpeed/raw/master/bbrplus/debian-ubuntu/x64/linux-image-4.14.129-bbrplus.deb
+
+      headurl=$(check_cn $headurl)
+      imgurl=$(check_cn $imgurl)
+
+      wget -O linux-headers.deb $headurl
+      wget -O linux-image.deb $imgurl
+
+      dpkg -i linux-image.deb
+      dpkg -i linux-headers.deb
+    else
+      echo -e "${Error} 不支持x86_64以外的系统 !" && exit 1
+    fi
+  fi
+
+  cd .. && rm -rf bbrplus
+  BBR_grub
+  echo -e "${Tip} 内核安装完毕，请参考上面的信息检查是否安装成功,默认从排第一的高版本内核启动"
+  check_kernel
+}
+
+#启用BBRplus
+startbbrplus() {
+  remove_bbr_lotserver
+  echo "net.core.default_qdisc=fq" >>/etc/sysctl.d/99-sysctl.conf
+  echo "net.ipv4.tcp_congestion_control=bbrplus" >>/etc/sysctl.d/99-sysctl.conf
+  sysctl --system
+  echo -e "${Info}BBRplus修改成功，重启生效！"
+}
+
+
